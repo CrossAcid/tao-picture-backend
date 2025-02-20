a70706491f2b12d52691d076b86e7b177fa0bec3
yjt <yujiangtao010315@163.com>
2025-02-20 15:54:20 +0800

    second

---
+++ src/main/java/com/crossacid/taopicturebackend/mapper/SpaceUserMapper.java
@@ -0,0 +1,18 @@
+package com.crossacid.taopicturebackend.mapper;
+
+import com.crossacid.taopicturebackend.model.entity.SpaceUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author JiangTaoYu
+* @description 针对表【space_user(空间用户关联)】的数据库操作Mapper
+* @createDate 2025-02-19 19:44:25
+* @Entity com.crossacid.taopicturebackend.model.entity.SpaceUser
+*/
+public interface SpaceUserMapper extends BaseMapper<SpaceUser> {
+
+}
+
+
+
+
